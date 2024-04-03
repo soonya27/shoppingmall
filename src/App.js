2f28709301a9128961dbb130804ea8a4f0c824aa
soyeong <psykor48@gmail.com>
2024-04-03 14:42:47 +0900

    context 폴더 이동

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './components/context/AuthContext';
 import Footer from './components/Footer/Footer';
-import { MediaQueryContextProvider } from './context/MediaQueryContext';
+import { MediaQueryContextProvider } from './components/context/MediaQueryContext';
 
 
 const queryClient = new QueryClient();
